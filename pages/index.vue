d50f9f83ac73894badf20568349406fa3cf85f7a
Nathan Azevedo <nathan@Nathans-MacBook-Pro.local>
2021-07-16 15:32:40 -0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <Hero />
+    <LargeCardDisplay  
+      v-for="cardInfo in largeCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+    <SmallCardDisplay 
+      v-for="cardInfo in smallCardInfo"
+      :key="cardInfo.id"
+      :cardsSection="cardInfo"
+    />
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from "@/assets/data.js"
+export default {
+  data(){
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
+    }
+  }
+}
+</script>
